17a45a9e04f796a4c7a7f760606e6ab5a9847faf
Martin <marb61a@gmail.com>
2020-12-14 20:20:28 +0000

    Updated Home page file

--- CucumberDressSearchProject/pages/Home.js
+++ CucumberDressSearchProject/pages/Home.js
@@ -10,12 +10,42 @@ class Home{
         return $(".btn.btn-default.button-search");
     }
 
+    get womenMenuOption(){
+        return $("ul.menu-content > li > a[title='Women']");
+    }
+
+    get dressesMenuOption(){
+        return $("ul.menu-content > li > a[title='Dresses']");
+    }
+
+    get tshirtMenuOption(){
+        return $("ul.menu-content > li > a[title='T-shirts']");
+    }
+
     // Search for a keyword
     search(keyword){
         this.searchField.waitForDisplayed(2000);
         this.searchField.setValue(keyword);
         this.searchButton.click();
     }
+
+    // Clicking a menu option
+    selectMenuOption(option){
+        switch(option){
+            case "Women":
+                this.womenMenuOption.click();
+                break;
+            case "Dresses":
+                this.dressesMenuOption.click();
+                break;
+            case "T-Shirts":
+                this.tshirtMenuOption.click();
+                break;
+            default:
+                console.log(`Invalid Option ${option}`);
+        }
+    }
+
 }
 
 module.exports = new Home();
