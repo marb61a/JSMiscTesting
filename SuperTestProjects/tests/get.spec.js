7e07a9e2c853ce64fec8c30b887c7deac6262d98
Martin <marb61a@gmail.com>
2020-12-16 19:32:42 +0000

    Added GET spec test file

---
+++ SuperTestProjects/tests/get.spec.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+const expect = require("chai").expect;
+
+const app = require("../src/app");
+
+describe("GET requests", () => {
+    it("get course id", () => {
+        request(app)
+            .get("/course/1")
+            .end((err, res) => {
+                expect(res.body.id).to.be.equal("1");
+            });
+    });
+
+    // localhost/course?name=mocha
+    it("get query param name", () => {
+        request(app)
+            .get("/course")
+            .query({"name": "mocha"})
+            .expect(200, {id: "1", name: ""});
+    });
+
+});
