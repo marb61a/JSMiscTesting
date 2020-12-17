a60835a11cbb0273aef36a8321818aa4cc203971
Martin <marb61a@gmail.com>
2020-12-17 11:48:07 +0000

    Added POST spec test file

---
+++ SuperTestProjects/tests/post.spec.js
@@ -0,0 +1,25 @@
+const request = require("supertest");
+const expect = require("chai").expect;
+
+const app = require("../src/app");
+
+describe("POST requests", () => {
+    it("json response", () => {
+        request(app)
+            .post("/course")
+            .send({ name : "supertest" })
+            .set('Accept', 'application/json')
+            .end((err, res) => {
+                expect(res.body.name).to.be.equal('supertest');
+            });
+    });
+
+    it("form response", () => {
+        request(app)
+            .post('/course')
+            .send('name=supertest')
+            .set('Accept', 'application/x-www-form-urlencoded')
+            .expect(200, {"id": "2", "name": "supertest"});
+    });
+
+});
