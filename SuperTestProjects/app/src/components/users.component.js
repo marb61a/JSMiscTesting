6957ab56f1ac5c74afeabd2cc2f75c03cb900973
Martin <marb61a@gmail.com>
2020-12-20 11:50:33 +0000

    Updated users component

--- SuperTestProjects/app/src/components/users.component.js
+++ SuperTestProjects/app/src/components/users.component.js
@@ -9,11 +9,38 @@ export default class Users extends Component {
         this.state = { usersCollection: [] };
     }
 
+    componentDidMount(){
+        axios.get("http://localhost:4000/users")
+            .then(res => {
+                this.setState({ usersCollection: res.data.users });
+            })
+            .catch(function(error){
+                console.log(error);
+            });
+    };
+
+    dataTable(){
+        return this.state.usersCollection.map((data, i) => {
+            return <DataTable obj={data} key={i} />;
+        });
+    };
+
     render(){
         return(
             <div className="wrapper-users">
                 <div className="container">
-
+                    <table className="table table-striped table-dark">
+                        <thead className="thead-dark">
+                            <tr>
+                                <td>ID</td>
+                                <td>Name</td>
+                                <td>Email</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.dataTable()}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         )
