28f0e75f52134d20f0c9ff3cdf5f802bd2fd4cfa
Martin <marb61a@gmail.com>
2020-12-20 11:36:57 +0000

    Updated crate user component

--- SuperTestProjects/app/src/components/create-user.component.js
+++ SuperTestProjects/app/src/components/create-user.component.js
@@ -5,15 +5,40 @@ export default class CreateUser extends Component {
     constructor(props){
         super(props);
 
+        this.state = {
+            name: '',
+            email: ''
+        };
     }
 
     onChangeUserName(e){
-        this.setState({ name: e.target.value })
+        this.setState({ name: e.target.value });
+    }
+
+    onChangeUserEmail(e){
+        this.setState({ email: e.target.value });
     }
 
     onSubmit(e){
         e.preventDefault();
 
+        const userObject = {
+            name: this.state.name,
+            email: this.state.email
+        };
+
+        axios.post("http://localhost:4000/users/create", userObject)
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        
+        this.setState({
+            name: '',
+            email: ''
+        });
     }
 
     render(){
