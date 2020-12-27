d8e37164983acd7ff8f943459015438f6b96f3cb
Martin <marb61a@gmail.com>
2020-12-27 12:01:47 +0000

    Moved code to external fixture file

--- playwright-folio-project/testrunner/inlinetestfixture.spec.ts
+++ playwright-folio-project/testrunner/inlinetestfixture.spec.ts
@@ -1,31 +1,4 @@
-import { folio } from "@playwright/test";
-import { devices } from "playwright";
-
-const fixtures = folio.extend();
-
-// Extending code will go here
-fixtures.browserOptions.override(async ({}, run) => {
-    await run({
-        headless: false,
-        slowMo: 2500,
-        // Will open the page with the browser devtools panel open
-        devtools: true
-    });
-
-});
-
-// Setting screen resolution
-// Tests may fail if using a combination of mobile devices and 
-// dev tools panel being set to true
-fixtures.contextOptions.override(async ({contextOptions}, run) => {
-    await run({
-        ...contextOptions,
-        ...devices["iPhone 11 Pro Max"]
-    });
-
-});
-
-const {it} = fixtures.build();
+import {it} from "./fixture";
 
 it("Runs a basic test of the neowin site", async({context}) => {
     var page = await context.newPage();
