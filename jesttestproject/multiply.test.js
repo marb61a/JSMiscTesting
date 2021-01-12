b78d324ea056e74b140220fa20b31d877f920467
Martin <marb61a@gmail.com>
2021-01-12 19:40:57 +0000

    Added basic jest test

---
+++ jesttestproject/multiply.test.js
@@ -0,0 +1,15 @@
+const multiply = require("./multiply");
+
+describe("test multiply positive scenarios", () => {
+    test("multiply 3*2 should equal to 6", () => {
+        expect(multiply(3,2)).toBe(6);
+    });
+
+});
+
+test("object example", () => {
+    const data = {first: 1};
+    data['second'] = 2;
+    expect(data).toEqual({first: 1, second: 2});
+    
+});
