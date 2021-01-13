18dafb0f6afa968a9ea4cbf44ab50b4aab2c94c7
Martin <marb61a@gmail.com>
2021-01-13 18:53:41 +0000

    Added asyncExample and shapshots test files

---
+++ jesttestproject/asyncExample.test.js
@@ -0,0 +1,14 @@
+// Assuming that fetchDataOverApi returns data from external api and function is called from another file
+const fetchDataOverApi = require("./fetchData.js");
+
+// Non async example
+// test("the user data for user 1", () => {
+//     const data = fetchDataOverApi();
+//     expect(data).toBe("John");
+// });
+
+// Async example
+test("the user data for user 1", async() => {
+    const data = await fetchDataOverApi();
+    expect(data).toBe("John");
+});
---
+++ jesttestproject/snapshots.test.js
@@ -0,0 +1,10 @@
+var itemStock = [
+    { 'Id': '1', 'ItemName': 'Razors', 'Stock': '10' },
+    { 'Id': '2', 'ItemName': 'Socks', 'Stock': '1'},
+    { 'Id': '3', 'ItemName': 'Towels', 'Stock': '100' },
+    { 'Id': '4', 'ItemName': 'Socks', 'Stock': '100'},
+];
+
+function filterItemStock(){
+    
+}
