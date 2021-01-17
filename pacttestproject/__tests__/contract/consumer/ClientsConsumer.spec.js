b4ecd6a75fa3d0c12123ee3aa3f204e535d4b18d
Martin <marb61a@gmail.com>
2021-01-17 12:17:34 +0000

    Updated ClientsConsumer consumer file

--- pacttestproject/__tests__/contract/consumer/ClientsConsumer.spec.js
+++ pacttestproject/__tests__/contract/consumer/ClientsConsumer.spec.js
@@ -22,5 +22,36 @@ describe("Clients Service", () => {
     }]
 
     afterEach(() => provider.verify());
-    
-})
+    describe("GET Clients", () => {
+        beforeEach(() => {
+            const interaction = {
+                state: "i have a list of cients",
+                uponReceiving: "a request for all clients",
+                withRequest: {
+                    method: "GET",
+                    path: "/clients",
+                    headers: {
+                        Accept: "application/json, text/plain, */*",
+                    }
+                }, 
+                willRespondWith: {
+                    status: 200,
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8",
+                    },
+                    body: GET_EXPECTED_BODY
+                }
+            }
+
+            return provider.addInteraction(interaction);
+        });
+
+        test("returns correct body, header and statusCode", async () => {
+            const response = await getClients();
+            expect(response.headers['content-type']).toBe("application/json; charset=utf-8");
+            expect(response.data).toEqual(GET_EXPECTED_BODY);
+            expect(response.status).toEqual(200);
+        })
+    });
+
+});
