fb800c26277618e44b2e979d369cc12d55df09f9
Martin <marb61a@gmail.com>
2021-01-18 11:41:54 +0000

    Added client spec provider file file

---
+++ pacttestproject/__tests__/contract/provider/ClientsProvider.spec.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const { Verifier } = require("@pact-foundation/pact");
+
+const { server, importData } = require("../../../src/provider");
+const SERVER_URL = "http://localhost:8081";
+
+server.listen(8081, () => {
+    importData();
+    console.log(`Clients Service listening on ${SERVER_URL}`);
+});
+
+describe("Clients Service Verification", () => {
+    it("validates the expectations of Client Service", () => {
+        let opts = {
+            provider: "Clients Service",
+            logLevel: "DEBUG",
+            providerBaseUrl: SERVER_URL,
+            pactUrls: [
+                
+            ]
+        }
+    });
+});
