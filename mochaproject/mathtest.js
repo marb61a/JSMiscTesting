a92ca5b5d94553f2a3a9ab94d21e4e50387e0b80
Martin <marb61a@gmail.com>
2021-01-21 19:33:48 +0000

    Added mocha folder and dependencies

---
+++ mochaproject/mathtest.js
@@ -0,0 +1,14 @@
+// Test Suite - Mathematical Operations
+
+// Test Cases
+
+// 1. Addition
+// 2. Subtraction
+// 3. Multiplication
+// 4. Division
+
+var assert = require('assert');
+
+describe("Mathematical Operations - Test Suite", function(){
+
+});
