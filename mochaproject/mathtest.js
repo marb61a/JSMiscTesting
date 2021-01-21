f49305222ecedcff527aff527053508f40fd383d
Martin <marb61a@gmail.com>
2021-01-21 19:43:29 +0000

    Updated math test file

--- mochaproject/mathtest.js
+++ mochaproject/mathtest.js
@@ -10,5 +10,36 @@
 var assert = require('assert');
 
 describe("Mathematical Operations - Test Suite", function(){
+    it("Adds two numbers", function(){
+        var a = 10;
+        var b = 10;
+        var c = a + b;
+
+        assert.equal(c, 20);
+    });
+
+    it("Subtracts two numbers", function(){
+        var a = 10;
+        var b = 10;
+        var c = a - b;
+
+        assert.equal(c, 0);
+    });
+
+    it("Multiplies two numbers", function(){
+        var a = 10;
+        var b = 10;
+        var c = a * b;
+
+        assert.equal(c, 100);
+    });
+
+    it("Multiplies two numbers", function(){
+        var a = 10;
+        var b = 10;
+        var c = a / b;
+
+        assert.equal(c, 1);
+    });
 
 });
