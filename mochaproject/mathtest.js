b0844c7d4d99c9c187236bf433e0279dc9d3344c
Martin <marb61a@gmail.com>
2021-01-22 12:12:57 +0000

    Updated math test file

--- mochaproject/mathtest.js
+++ mochaproject/mathtest.js
@@ -10,13 +10,35 @@
 var assert = require('assert');
 
 describe("Mathematical Operations - Test Suite", function(){
-    it("Adds two numbers", function(){
+    this.timeout(5000);
+
+    // it.only("Adds two numbers", function(){
+    //     var a = 20;
+    //     var b = 10;
+    //     var c = a + b;
+
+    //     assert.equal(c, 30);
+    // });
+
+    it("Adds two numbers", function(done){
+        // Will fail due to timeout issues
+        this.timeout(500);
+        setTimeout(done, 3000);
+
         var a = 10;
         var b = 10;
         var c = a + b;
 
         assert.equal(c, 20);
     });
+
+    // it.skip("Subtracts two numbers", function(){
+    //     var a = 20;
+    //     var b = 10;
+    //     var c = a - b;
+
+    //     assert.equal(c, 10);
+    // });
 
     it("Subtracts two numbers", function(){
         var a = 10;
